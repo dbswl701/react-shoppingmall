2270c4f20f77eb0b319d7373895c7f73eebe97bc
KangYunJi <dbswl701@ajou.ac.kr>
2023-09-19 22:58:01 +0900

    feat: 라우팅 구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { Route, Routes, Outlet } from 'react-router-dom';
 import './App.css';
+import Main from './pages/main';
+import Login from './pages/login';
+import Detail from './pages/detail';
+import Busket from './pages/basket';
+
+function Layout () {
+  return (
+    <div>
+      {/* <Nav /> */}
+      Nav
+      <Outlet />
+      {/* <Footer /> */}
+      Footer
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      초기세팅
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Main />}/>
+          <Route path="login" element={<Login />} />
+          <Route path="/item/:itemId" element={<Detail />} />
+          <Route path="/busket" element={<Busket />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/basket/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Busket() {
+  return (
+    <div>Busket</div>
+  )
+}
