6cf7e9db5763e86cbfb1339bd9722e7f7b4ba277
KangYunJi <dbswl701@ajou.ac.kr>
2023-09-21 11:52:37 +0900

    feat: 로그인 페이지 구현

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -3,8 +3,10 @@ import { ReactComponent as Cart } from "../asests/icons/cart.svg";
 import { ReactComponent as Private } from "../asests/icons/private.svg";
 import { ReactComponent as Login } from "../asests/icons/login.svg";
 import { ReactComponent as Logout } from "../asests/icons/logout.svg";
+import { useNavigate } from 'react-router-dom';
 
 export default function Nav() {
+  const navigate = useNavigate();
   return (
     <nav style={{display: 'flex', justifyContent: 'space-between', padding: '0px 20px', height: '65px', boxShadow: 'rgba(0, 0, 0, 0.45) 0px 25px 20px -20px'}}>
       <div>
@@ -13,7 +15,7 @@ export default function Nav() {
       <div style={{display: 'flex', alignItems: 'center', gap: '20px'}}>
         <Cart height="24px" />
         <Private width="24px" height="24px" />
-        <Login width="24px" height="24px" />
+        <Login width="24px" height="24px" onClick={() => navigate('./login')} />
         <Logout width="24px" height="24px" />
       </div>
     </nav>
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,7 +1,38 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 
+const Input = styled.input`
+  width: 300px;
+  height: 35px;
+  border-radius: 5px;
+  margin-bottom: 10px;
+  padding-left: 10px;
+  &:focus {
+    background-color: rgb(228, 235, 248);
+  }
+`;
+
+const Button = styled.button`
+  background-color: rgb(117, 118, 131);
+  color: white;
+  height: 45px;
+  cursor: pointer;
+  margin-top: 10px;
+`;
 export default function Login() {
+  const navigate = useNavigate();
   return (
-    <div>Login</div>
+    <div style={{display: 'flex', justifyContent: 'center', marginTop: '150px'}}>
+      <div style={{display: 'flex', flexDirection: 'column', width: '450px', justifyContent: 'center', alignItems: 'center', boxShadow: 'rgba(0, 0, 0, 0.35) 0px 5px 15px', padding: '20px 10px'}}>
+        <h1>로그인</h1>
+        <form style={{display: 'flex', flexDirection: 'column'}}>
+          <Input placeholder='Email' />
+          <Input type="password" placeholder='Password'/>
+          <Button>로그인</Button>
+        </form>
+        <p><b>계정이 없습니까?</b><span onClick={() => navigate('../singup')}>  가입하기</span></p>
+      </div>
+    </div>
   )
 }
