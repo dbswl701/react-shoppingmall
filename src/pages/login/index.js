1afb3e24bff9f87201fc83ca4f9d02af95ba6d2a
KangYunJi <dbswl701@ajou.ac.kr>
2023-09-21 15:07:26 +0900

    feat: 회원가입 유효성 검사 기능 추가

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -6,7 +6,7 @@ const Input = styled.input`
   width: 300px;
   height: 35px;
   border-radius: 5px;
-  margin-bottom: 10px;
+  margin-top: 10px;
   padding-left: 10px;
   &:focus {
     background-color: rgb(228, 235, 248);
@@ -18,7 +18,8 @@ const Button = styled.button`
   color: white;
   height: 45px;
   cursor: pointer;
-  margin-top: 10px;
+  margin-top: 20px;
+  width: 316px;
 `;
 export default function Login() {
   const navigate = useNavigate();
@@ -27,11 +28,9 @@ export default function Login() {
     <div style={{display: 'flex', justifyContent: 'center', marginTop: '150px'}}>
       <div style={{display: 'flex', flexDirection: 'column', width: '450px', justifyContent: 'center', alignItems: 'center', boxShadow: 'rgba(0, 0, 0, 0.35) 0px 5px 15px', padding: '20px 10px'}}>
         <h1>로그인</h1>
-        <form style={{display: 'flex', flexDirection: 'column'}}>
-          <Input placeholder='Email' />
-          <Input type="password" placeholder='Password'/>
-          <Button>로그인</Button>
-        </form>
+        <Input placeholder='Email' />
+        <Input type="password" placeholder='Password'/>
+        <Button>로그인</Button>
         <p><b>계정이 없습니까?</b><span onClick={() => navigate('../signup')}>  가입하기</span></p>
       </div>
     </div>
--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -6,7 +6,7 @@ const Input = styled.input`
   width: 300px;
   height: 35px;
   border-radius: 5px;
-  margin-bottom: 10px;
+  margin-top: 10px;
   padding-left: 10px;
   &:focus {
     background-color: rgb(228, 235, 248);
@@ -18,7 +18,15 @@ const Button = styled.button`
   color: white;
   height: 45px;
   cursor: pointer;
-  margin-top: 10px;
+  margin-top: 20px;
+  width: 316px;
+`;
+
+const InfoText = styled.p`
+  margin: 0px;
+  font-size: 13px;
+  color: #d52e2e;
+  margin-top: 2px;
 `;
 export default function Signup() {
   const navigate = useNavigate();
@@ -27,23 +35,71 @@ export default function Signup() {
     pw: '',
     pwCheck: '',
   })
+  const [state, setState] = useState({
+    idIsValid: true,
+    pwIsValid: true,
+    pwCheck: true,
+  })
   const handleChange = (e) => {
     const {name, value} = e.target;
     console.log(name, value);
-    setInfo({...info, [name]: value});
+    setInfo({...info, [name]: value}); // 잉 비번 복붙해서 pwCheck에 넣으면 input name 이 복사되네...?
   }
 
+  const fetchData = async () => {
+
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // 한 함수에서 setState 여러번 사용하면 최종본만 반영되는듯 -> 변수 따로 지정해서 저장한 다음 마지막에 set해보자
+    let idIsValid = true;
+    let pwIsValid = true;
+    let pwCheck = true;
+
+    // 이메일 유효성 검사
+    const emailRegExp = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
+    if (!emailRegExp.test(info.id)) idIsValid = false;
+    else idIsValid = true;
+
+    // 비밀번호 유효성 검사
+    const pwRegExp = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
+    if (!pwRegExp.test(info.pw)) pwIsValid = false;
+    else pwIsValid = true;
+
+    // 비밀번호 확인
+    if (info.pw !== info.pwCheck) pwCheck = false;
+    else pwCheck = true;
+
+    // 유효성 검사 후, 서버에 요청
+    if(idIsValid && pwIsValid && pwCheck) {
+      console.log(1111111);
+      fetchData();
+    }
+
+    setState({...state, pwIsValid, idIsValid, pwCheck});
+  }
+  console.log(state);
+  console.log(info);
   return (
     <div style={{display: 'flex', justifyContent: 'center', marginTop: '150px'}}>
       <div style={{display: 'flex', flexDirection: 'column', width: '450px', justifyContent: 'center', alignItems: 'center', boxShadow: 'rgba(0, 0, 0, 0.35) 0px 5px 15px', padding: '20px 10px'}}>
         <h1>회원가입</h1>
-        <form style={{display: 'flex', flexDirection: 'column'}}>
-          <Input name="id" value={info.id} onChange={handleChange} placeholder='Email' />
+        <Input name="id" value={info.id} onChange={handleChange} placeholder='Email' />
+          {
+            !state.idIsValid && <InfoText>유효한 이메일 형식이 아닙니다.</InfoText>
+          }
           <Input name="pw" value={info.pw} onChange={handleChange} type="password" placeholder='Password'/>
+          {
+            !state.pwIsValid && <InfoText>영문자, 숫자, 특수문자를 사용해 8~25자의 비밀번호를 입력해주세요.</InfoText>
+          }
           <Input name="pwCheck" value={info.pwCheck} onChange={handleChange} type="password" placeholder='Password Check'/>
-          <Button>로그인</Button>
-        </form>
-        <p><b>계정이 없습니까?</b><span onClick={() => navigate('../signup')}>  가입하기</span></p>
+          {
+            !state.pwCheck && <InfoText>비밀번호를 확인해주세요.</InfoText>
+          }
+          <Button onClick={handleSubmit}>회원가입</Button>
+        <p><b>계정이 있습니까?</b><span onClick={() => navigate('../signup')}>  로그인하기</span></p>
       </div>
     </div>
   )
