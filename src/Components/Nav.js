86f2cb7ab1044ac0ffb7ba879f587ef8ca582584
KangYunJi <dbswl701@ajou.ac.kr>
2023-09-25 23:12:15 +0900

    feat: 네비게이션 바 클릭 시 링크 이동 기능 추가

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -10,10 +10,10 @@ export default function Nav() {
   return (
     <nav style={{display: 'flex', justifyContent: 'space-between', padding: '0px 20px', height: '65px', boxShadow: 'rgba(0, 0, 0, 0.45) 0px 25px 20px -20px'}}>
       <div>
-        <h1 style={{color: 'rgb(63,31,96)', marginLeft: '20px', margin: '0px', lineHeight: '65px'}}>Shop</h1>
+        <h1 style={{color: 'rgb(63,31,96)', marginLeft: '20px', margin: '0px', lineHeight: '65px', cursor: 'pointer'}} onClick={() => navigate('./#')}>Shop</h1>
       </div>
       <div style={{display: 'flex', alignItems: 'center', gap: '20px'}}>
-        <Cart height="24px" />
+        <Cart height="24px" onClick={() => navigate('./basket')}/>
         <Private width="24px" height="24px" />
         <Login width="24px" height="24px" onClick={() => navigate('./login')} />
         <Logout width="24px" height="24px" />
