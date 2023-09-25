650335f4f556d349b4b0a27f227ef24c246070fc
KangYunJi <dbswl701@ajou.ac.kr>
2023-09-25 23:06:21 +0900

    feat: 장바구니 계산하기 기능 추가 및 관련 에러 해결

--- src/pages/basket/index.js
+++ src/pages/basket/index.js
@@ -3,6 +3,7 @@ import firebase from '../../firebase'; // Firebase 모듈 가져오기
 import { ReactComponent as Cart } from '../../asests/icons/cartBig.svg';
 import styled from 'styled-components';
 import Item from './Item';
+import {ReactComponent as Snipper} from '../../asests/icons/snipper.svg';
 
 const Title = styled.h1`
   font-size: 3rem;
@@ -22,8 +23,20 @@ const TotalCostBtn = styled.div`
   justify-content: center;
 `;
 
+const CalBtn = styled.button`
+  border: 1px solid rgb(160,160,167);
+  background-color: transparent;
+  width: 150px;
+  cursor: pointer;
+  &:hover {
+    background-color: rgb(114, 116, 129);
+    color: white;
+  }
+
+`;
 export default function Basket() {
-  const [carts, setCarts] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [carts, setCarts] = useState(null);
   const uid = JSON.parse(localStorage.getItem('user')).uid;
   const [totalCost, setTotalCost] = useState(0); // 이거 그냥 변수로 선언 안하고 state로 선언하는게 맞나...?
   console.log(uid);
@@ -31,11 +44,20 @@ export default function Basket() {
   useEffect(() => {
     const getData = async () => {
       try{
-        const starCountRef = firebase.database().ref(`users/${uid}/carts`);
-        starCountRef.on('value', (snapshot) => {
+        const CartsRef = firebase.database().ref(`users/${uid}/carts`);
+        console.log(CartsRef);
+        await CartsRef.on('value', (snapshot) => {
           const data = snapshot.val();
+          if (data === null) {
+            console.log('삭제??')
+            setCarts(null);
+            return;
+          }
           console.log(data);
-          setTotalCost(Object.values(data).reduce((prev, cur) => prev.price*prev.count + cur.price*cur.count))
+          console.log(Object.keys(data));
+          console.log(Object.values(data));
+          console.log(Object.values(data).reduce((prev, cur) => prev + cur.price * cur.count, 0));
+          setTotalCost(Object.values(data).reduce((prev, cur) => prev + cur.price * cur.count, 0))
           setCarts(data);
         });
       }
@@ -43,38 +65,64 @@ export default function Basket() {
         console.error(err);
       }
     }
-    getData();
-  }, [])
+    setIsLoading(true);
+    getData(); // firebase는 비동기적으로 동작안하나...?
+    setIsLoading(false);
 
-  const noItem = (
-    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '30px'}}>
-      {/* <h1>장바구니</h1> */}
-      <Cart width="400" />
-      <Title>Cart가 비어있습니다.</Title>
-      <h2>Cart에 상품을 넣어주세요.</h2>
-      <Text>계속 쇼핑하기</Text>
-    </div>
-  )
-  const printItems = (
-    <div>
-      <h1>장바구니</h1>
-      <div style={{display: 'flex', flexDirection: 'column', gap:'20px'}}>
-        {
-          Object.values(carts).map(item => <Item key={item.id} item={item} />)
-        }
+  }, [])
+  console.log(carts);
+  console.log(totalCost)
+  const handleCalculate = () => {
+    // db에서 carts 다 지우기
+    const removeData = async () => {
+      try{
+        const CartsRef = await firebase.database().ref(`users/${uid}/carts/`).set([]);
+      }
+      catch(err) {
+        console.error(err);
+      }
+    }
+    removeData();
+  }
+
+  const noItem = () => {
+    return (
+      <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '30px'}}>
+        {console.log(22222)}
+
+        {/* <h1>장바구니</h1> */}
+        <Cart width="400" />
+        <Title>Cart가 비어있습니다.</Title>
+        <h2>Cart에 상품을 넣어주세요.</h2>
+        <Text>계속 쇼핑하기</Text>
       </div>
-      <div style={{display: 'flex', justifyContent: 'end', gap: '20px', marginTop: '50px'}}>
-        <TotalCostBtn>
-          <p style={{fontWeight: '600'}}>합계: $ {totalCost.toFixed(2)}</p>
-        </TotalCostBtn>
-        {/* div 밑에 이렇게 div로 감싸지 않은 button이나 Img 넣는게 바람직한가? */}
-        <button style={{border: '1px solid rgb(160,160,167)', backgroundColor: 'transparent', width: '150px'}}>계산하기</button>
+    )
+  };
+  const printItems = () => { // 함수가 아닌 변수로 선언하면 일단 평가되기에 한번 실행된다 -> 에러 발생
+    return (
+      <div>
+        {console.log(11111)}
+        {console.log(carts)}
+        <h1>장바구니</h1>
+        <div style={{display: 'flex', flexDirection: 'column', gap:'20px'}}>
+          {
+            Object.values(carts).map(item => <Item key={item.id} item={item} />)
+          }
+        </div>
+        <div style={{display: 'flex', justifyContent: 'end', gap: '20px', marginTop: '50px'}}>
+          <TotalCostBtn>
+            <p style={{fontWeight: '600'}}>합계: $ {totalCost.toFixed(2)}</p>
+          </TotalCostBtn>
+          {/* div 밑에 이렇게 div로 감싸지 않은 button이나 Img 넣는게 바람직한가? */}
+          <CalBtn onClick={handleCalculate}>계산하기</CalBtn>
+        </div>
       </div>
-    </div>
-  )
+    )
+  };
+
   return (
     <div style={{display: 'flex', justifyContent: 'center'}}>
-      {carts === undefined ? noItem : printItems}
+      {isLoading ? <Snipper style={{display: 'flex', margin: '100px auto'}} /> : carts === null ? noItem() : printItems()}
     </div>
   )
 }
