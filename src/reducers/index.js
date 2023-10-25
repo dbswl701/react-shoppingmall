88d119d4eb4b56b3f823073f2a25c2fc45a49d6d
KangYunJi <dbswl701@ajou.ac.kr>
2023-10-25 23:53:55 +0900

    feat: 로딩에 관한 리덕스 추가

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import items from './items';
 import user from './user';
+import loading from './loading';
 
 const rootReducer = combineReducers({
   items,
-  user
+  user,
+  loading
 });
 
 export default rootReducer;
